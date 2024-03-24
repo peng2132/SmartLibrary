208bbcc4eac35b77e3caed94b5771164ba1101c6
peng2132.github.io <zydx520@gmail.com>
2024-03-24 20:46:19 +0800

    上传

---
+++ src/main/java/com/example/smartlibrary/SmartLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartLibraryApplication.class, args);
+    }
+
+}
